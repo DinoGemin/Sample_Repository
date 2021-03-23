06f3a93ff2c13e443242c408fd3fd46ba6c110bc
Dino Gemin <dgemin@gmail.com>
2021-03-23 18:29:18 +0100

    modifica 3

--- ProgettoProva/src/it/test/Prova.java
+++ ProgettoProva/src/it/test/Prova.java
@@ -2,6 +2,7 @@ package it.test;
 
 public class Prova {
 	// Test
-	String Name = "WS_GIT_user1";
-	String Name = "ImmagineSample";
+	String Name1 = "WS_GIT_user1";
+	String Name2 = "ImmagineSample";
+	String Name3 = "Merge";
 }
