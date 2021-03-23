4a863cbd04e0932f814070c7c6184f39f5a54a39
Dino Gemin <dgemin@gmail.com>
2021-03-23 19:05:52 +0100

    salva in branch

--- ProgettoProva/src/it/test/Prova.java
+++ ProgettoProva/src/it/test/Prova.java
@@ -6,5 +6,29 @@ public class Prova {
 	String Name2 = "ImmagineSample";
 	String Name3 = "Mergeffff";
 	String Name4 = "From1_to_2";
+	private String getName1() {
+		return Name1;
+	}
+	private void setName1(String name1) {
+		Name1 = name1;
+	}
+	private String getName2() {
+		return Name2;
+	}
+	private void setName2(String name2) {
+		Name2 = name2;
+	}
+	private String getName3() {
+		return Name3;
+	}
+	private void setName3(String name3) {
+		Name3 = name3;
+	}
+	private String getName4() {
+		return Name4;
+	}
+	private void setName4(String name4) {
+		Name4 = name4;
+	}
 	
 }
